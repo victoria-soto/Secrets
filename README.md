5647a9c1781485c5d7b250a6ca86c1e275051eae
Victoria Soto <vjaramil@hawaii.edu>
2021-08-15 20:42:58 -1000

    Level 1 - Username and Password Only

--- README.md
+++ README.md
@@ -4,7 +4,8 @@ Similar to [Whisper](http://whisper.sh/), Secrets is a secret posting applicatio
 
 The project demonstrates six levels of authentication and security using various [NPM](https://www.npmjs.com/) packages and OAuth 2.0 sign in with Google.
 
-## Installation
+## Initial Set-up
+Creates the app before any authentication/security measures implemented.
 
 ### Create package.json
 
@@ -17,7 +18,13 @@ $ npm init -y
 $ npm i express ejs body-parser
 ```
 
-## Level 1
+## Level 1 - User Account Database
+Contains a database of user accounts and stores newly created accounts using [Mongoose](https://mongoosejs.com/) and [mongoDB](https://www.mongodb.com/).
+
+### Install Mongoose
+```sh
+$ npm i mongoose
+```
 
 ## Level 2
 
--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 
+// mongoose package
+const mongoose = require("mongoose");
 
 app.use(express.static("public"));
 
@@ -10,8 +12,18 @@ app.use(express.static("public"));
 app.use('/favicon.ico', express.static('public/images/favicon.ico'));
 
 app.set("view engine", "ejs");
-
 app.use(bodyParser.urlencoded({extended: true}));
+
+// connect to mongoose db
+mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true, useUnifiedTopology: true});
+
+// create userDB
+const userSchema = {
+  email: String,
+  password: String
+};
+
+const User = new mongoose.model("User", userSchema);
 
 // render home
 app.get("/", function(req, res){
@@ -23,9 +35,43 @@ app.get("/register", function(req, res){
   res.render("register");
 });
 
+// post method to register new users
+app.post("/register", function(req, res){
+  const newUser = new User({
+    email: req.body.username,
+    password: req.body.password
+  });
+
+  newUser.save(function(err){
+    if(err){
+      console.log(err);
+    } else {
+      res.render("secrets");
+    }
+  });
+});
+
 // render login
 app.get("/login", function(req, res){
   res.render("login");
+});
+
+// post method to check credentials of existing users
+app.post("/login", function(req, res){
+  const username = req.body.username;
+  const password = req.body.password;
+
+  User.findOne({email:username}, function(err, foundUser){
+    if(err) {
+      console.log(err);
+    } else {
+      if(foundUser) {
+        if(foundUser.password === password) {
+          res.render("secrets");
+        }
+      }
+    }
+  });
 });
 
 app.listen(3000, function(req, res){
